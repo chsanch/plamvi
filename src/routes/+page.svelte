e006e88757f1b598f3084f0afaf38aa17d938f75
Christian Sánchez <chsanch@pm.me>
2024-08-05 20:23:22 +0200

    add video demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import * as Dialog from '$lib/components/ui/dialog';
+</script>
+
 <div
 	class="flex h-full min-h-[calc(100vh-64px)] w-screen flex-col justify-between bg-[url('/landing/background.png')] bg-cover"
 >
@@ -7,21 +11,32 @@
 			>Selecciona el tipo de IA con el que deseas realizar el itinerario e introduce el destino y
 			los ajustes que quieras para personalizarlo. Clica en "Viaje" para empezar y ¡Buen viaje!</span
 		>
+		
+		<span class="w-full text-sm text-muted-foreground sm:w-1/2 sm:text-xl"> Si quieres ver como funciona, aquí tienes un video:
+		<Dialog.Root>
+			<Dialog.Trigger class="font-bold underline">Ver Demo</Dialog.Trigger>
+			<Dialog.Content class="w-full max-w-5xl">
+				<video controls src="/demo_plamvi.mp4">
+					<track kind="captions" />
+				</video>
+			</Dialog.Content>
+		</Dialog.Root>
+			</span>
 	</div>
 
 	<div>
 		<h2
-			class="font-heading flex flex-wrap overflow-hidden text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
+			class="flex flex-wrap overflow-hidden font-heading text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
 		>
 			Planificador
 		</h2>
 		<h2
-			class="font-heading flex flex-wrap overflow-hidden text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
+			class="flex flex-wrap overflow-hidden font-heading text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
 		>
 			de itinerarios
 		</h2>
 		<h2
-			class="font-heading flex flex-wrap overflow-hidden text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
+			class="flex flex-wrap overflow-hidden font-heading text-6xl font-bold opacity-50 sm:text-8xl md:text-[9rem] xl:text-[14rem]"
 		>
 			con IA
 		</h2>
